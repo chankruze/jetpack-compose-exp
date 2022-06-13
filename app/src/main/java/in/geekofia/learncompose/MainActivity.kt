7186ed26d3f5781ce977ae2f59dc0ef75563ded2
chankruze <chankruze@gmail.com>
2022-06-13 11:39:31 +0530

    add column and row example
    Signed-off-by: chankruze <chankruze@gmail.com>

--- app/src/main/java/in/geekofia/learncompose/MainActivity.kt
+++ app/src/main/java/in/geekofia/learncompose/MainActivity.kt
@@ -3,7 +3,6 @@ package `in`.geekofia.learncompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -12,8 +11,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import `in`.geekofia.learncompose.ui.theme.LearnComposeTheme
 import `in`.geekofia.learncompose.ui.theme.Typography
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
@@ -23,14 +23,27 @@ class MainActivity : ComponentActivity() {
             LearnComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
                     color = MaterialTheme.colors.background
                 ) {
                     Column(
-                        Modifier.padding(16.dp)
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
                     ) {
                         Greeting("Chandan")
+                        CustomItem(weight = 1f, color = MaterialTheme.colors.secondary)
+                        Row(
+                            modifier = Modifier.weight(1f),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.SpaceEvenly
+                        ) {
+                            CustomItem(weight = 1f, color = MaterialTheme.colors.error)
+                            CustomItem(weight = 1f)
+                        }
                     }
+
                 }
             }
         }
@@ -45,12 +58,43 @@ fun Greeting(name: String) {
     )
 }
 
+@Composable
+fun ColumnScope.CustomItem(weight: Float, color: Color = MaterialTheme.colors.primary) {
+    Surface(
+        modifier = Modifier
+            .width(200.dp)
+            .weight(weight),
+        color = color
+    ) {}
+}
+
+@Composable
+fun RowScope.CustomItem(weight: Float, color: Color = MaterialTheme.colors.primary) {
+    Surface(
+        modifier = Modifier
+            .height(200.dp)
+            .weight(weight),
+        color = color
+    ) {}
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     LearnComposeTheme {
-        Column {
-            Greeting("Chandan")
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            CustomItem(weight = 3f, color = MaterialTheme.colors.secondary)
+            CustomItem(weight = 1f)
+        }
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            CustomItem(weight = 3f, color = MaterialTheme.colors.secondary)
+            CustomItem(weight = 1f)
         }
     }
 }
